32b6e3d1246f78954487de0f16fabd923ebc1d90
RaspberryCrumble <raspberryCrumble@outlook.com>
2016-01-14 22:45:10 +0000

    first commit
    	new file:   my_pwm.py

---
+++ my_pwm.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+led=11
+GPIO.setup(led,GPIO.OUT)
+my_pwm = GPIO.PWM(led,100)
+my_pwm.start(0)
+try:
+  while(True):
+     brightness=input("How bright? (1-67) ")
+     print (str(2**brightness))
+     my_pwm.ChangeDutyCycle(2**brightness)
+except KeyboardInterrupt:
+  print("InteruptedOK")
+  my_pwm.stop()
+  GPIO.cleanup()
+   
